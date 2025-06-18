066a8741e65129a0926453d0f6414c79cfc9fe3f
Upasana Mukherjee <upasanadesigns03@gmail.com>
2025-06-18 02:48:39 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3+3) ->we are using nodejs and not browser
+
+//console.log(3+3); console.log("mio") // code readability is bad
+
+let name ="mio" //string data type
+let age= 22 //number data type
+let isLoggedIn = false //boolean data type
+let state; //undefined (value is not assigned)
+let state1=null; //null type => empty
+
+// number =>2^52
+//bigint for bigger numbers
+//string =>""
+//boolean=> true or false
+//null => standalone value in js
+//undefined => is a type itself
+//symbol=> unique
+
+//object
+
+console.log(typeof "mio");  //obtain the data type of an element
+console.log(typeof null);  // returns object
+console.log(typeof undefined);  //returns undefined
