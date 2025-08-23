0bf922b74eb3c44f1b702cc561953ce46c8f6a62
Upasana Mukherjee <upasanadesigns03@gmail.com>
2025-08-23 08:41:37 +0000

    areays

---
+++ 02_arrays/01_arrays.js
@@ -0,0 +1,11 @@
+//array
+
+//can contain different types of elements
+//js array are resizeable
+//copy operation -> shallow copy (share same refernece point)
+const myArr = [1, 2, 3, 4, 5]
+const myArr1 = [1, 2, 3, 4, 5, true, Mio]
+console.log(myArr[0]);
+
+
+
