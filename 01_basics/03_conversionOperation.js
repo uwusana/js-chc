e3c15988668cbcd9f192538609558b4f266aed5d
Upasana Mukherjee <upasanadesigns03@gmail.com>
2026-02-07 19:25:02 +0530

    minor comment changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,23 +3,24 @@ let score1 ="33abc"
 let value = null
 let value1 = undefined
 
-// console.log(typeof score); //done for the convenience of frontend to backend communication
-// console.log(typeof score1);
-// console.log(typeof value);
-// console.log(typeof value1);
+console.log(typeof score); //done for the convenience of frontend to backend communication
+console.log(typeof score1);
+console.log(typeof value);
+console.log(typeof value1);
+console.log("============================");
 
 let valueInNumber = Number(score)
 let valueInNumber1 = Number(score1)
 let valueOfNull = Number(value)
 let valueOfUndefined = Number(value1) 
 
-// console.log(typeof valueInNumber);
-// console.log(typeof valueInNumber1); //returns number but it is incorrect
-// console.log( valueInNumber1); //returns NaN -> noy a number
-// console.log(typeof valueOfNull);
-// console.log(valueOfNull); //returns 0
-// console.log(valueOfUndefined); //NaN
-
+console.log(typeof valueInNumber);
+console.log(typeof valueInNumber1); //returns number but it is incorrect
+console.log( valueInNumber1); //returns NaN -> noy a number
+console.log(typeof valueOfNull);
+console.log(valueOfNull); //returns 0
+console.log(valueOfUndefined); //NaN
+console.log("============================");
 //notes
 /*
 "33"-> 33
@@ -30,8 +31,8 @@ true -> 1 and false -> 0
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn); //returns true
-
+console.log(booleanIsLoggedIn); //returns true
+console.log("============================");
 //1-> true and 0 -> false
 //"" -> false
 //"string" -> true
@@ -39,32 +40,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(typeof stringNumber);
-// console.log(stringNumber);
-
+console.log(typeof stringNumber);
+console.log(stringNumber);
+console.log("============================");
 // ********************************Operations**********************************
 
 let val = 3
 let negVal =-val
-//console.log(negVal);
+console.log(negVal);
 
-// console.log(2+2);
-// console.log(2*2);
-// console.log(2**3);
-// console.log(2%3);
-// console.log(2/3);
-
+console.log(2+2);
+console.log(2*2);
+console.log(2**3);
+console.log(2%3);
+console.log(2/3);
+console.log("============================");
 let str1 ="hello"
 let str2 = " mio"
 
 let str3 = str1 + str2
-// console.log(str3);
+console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2"); //output: 12
-// console.log("1" + 2 + 2); //output:122
-// console.log(1 + 2 + "2"); //output:32 special ECMA language dependent
-
+console.log("1" + 2);
+console.log(1 + "2"); //output: 12
+console.log("1" + 2 + 2); //output:122
+console.log(1 + 2 + "2"); //output:32 special ECMA language dependent
+console.log("============================");
 // console.log(3+4*5%3); //not recommended for dev work
 
 // console.log(true);
